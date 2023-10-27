04fa522493664c14ad012582775d69edf9534cca
Victor Sepulveda <victorseva123@gmail.com>
2023-10-27 06:34:36 -0500

    Actualizado 27/10/23

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,44 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
-    name: {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name:{
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height_min:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    weight_min:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    height_max:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    weight_max:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    life_span:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    image:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    createdInDB: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
